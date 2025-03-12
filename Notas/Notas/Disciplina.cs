7a2d2dbbb79deec68bb6516f21d7634f1535b898
ghabryeu <gabiconstantino2001@gmail.com>
2025-03-12 14:59:03 -0300

    Exercício 2 da lista de classes

---
+++ Notas/Notas/Disciplina.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Disciplina
+{
+    public string Nome { get; set; }
+    public int MediaCurso { get; set; }
+    List<Aluno> Alunos { get; set; } = new List<Aluno>();
+    Dictionary<Aluno, List<int>> Notas { get; set; } = new Dictionary<Aluno, List<int>>();
+
+    public void AdicionarAluno(Aluno aluno)
+    {
+        Alunos.Add(aluno);
+        Notas[aluno] = new List<int>();
+    }
+
+    public void AdicionarNota(Aluno aluno, int nota)
+    {
+        Notas[aluno].Add(nota);
+    }
+
+    public int CalcularMedia(Aluno aluno)
+    {
+        int soma = Notas[aluno].Sum();
+        return soma / Notas[aluno].Count;
+    }
+
+    public bool AprovarReprovar(Aluno aluno)
+    {
+        if(CalcularMedia(aluno) < MediaCurso)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    public void ImprimirDados()
+    {
+        Console.WriteLine("Nome\tDisciplina\tMédia final\tAprovado");
+
+        foreach(var aluno in Alunos)
+        {
+            Console.WriteLine($"{aluno.Nome}\t{Nome}\t{CalcularMedia(aluno)}\t{AprovarReprovar(aluno)}");
+        }
+
+    }
+}
---
+++ Notas/Notas/Program.cs
@@ -0,0 +1,25 @@
+﻿Disciplina disciplina = new Disciplina();
+Aluno aluno1 = new Aluno();
+Aluno aluno2 = new Aluno();
+
+
+disciplina.Nome = "cálculo";
+disciplina.MediaCurso = 7;
+
+aluno1.Nome = "gabs";
+aluno2.Nome = "ricardo";
+
+disciplina.AdicionarAluno(aluno1);
+disciplina.AdicionarAluno(aluno2);
+
+disciplina.AdicionarNota(aluno1, 8);
+disciplina.AdicionarNota(aluno1, 6);
+
+disciplina.AdicionarNota(aluno2, 5);
+disciplina.AdicionarNota(aluno2, 5);
+
+disciplina.CalcularMedia(aluno1);
+disciplina.CalcularMedia(aluno2);
+
+disciplina.ImprimirDados();
+
